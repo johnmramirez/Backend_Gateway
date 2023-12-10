ebcd5cf30f9c1badd7909f02a85275c09300da50
johnmramirez <johnmichaelramirez7@gmail.com>
2023-12-10 17:35:14 -0600

    added discovery service to cloud gateway, updated properties

--- src/main/java/com/practice/cloud_gateway/RestaurantReviewGatewayApplication.java
+++ src/main/java/com/practice/cloud_gateway/RestaurantReviewGatewayApplication.java
@@ -3,11 +3,13 @@ package com.practice.cloud_gateway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RestaurantReviewGatewayApplication {
 
 	@Value("${cloud_gateway.forwardToService}")
