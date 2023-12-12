2f7d6e27deec2c0748bffe60221b217976ed17ec
johnmramirez <johnmichaelramirez7@gmail.com>
2023-12-12 12:00:06 -0600

    updated filter to add "application/json" header to forwarded request

--- src/main/java/com/practice/cloud_gateway/filter/LoggingGlobalPreFilter.java
+++ src/main/java/com/practice/cloud_gateway/filter/LoggingGlobalPreFilter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -18,6 +19,12 @@ public class LoggingGlobalPreFilter implements GlobalFilter {
     public Mono<Void> filter(
             ServerWebExchange exchange,
             GatewayFilterChain chain) {
+
+        ServerHttpRequest request = exchange.getRequest()
+                .mutate()
+                .header("Content-Type", "application/json")
+                .build();
+
         logger.info("Global Pre Filter executed");
         return chain.filter(exchange);
     }
